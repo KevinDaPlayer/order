fd12c5d13693a94fdb9808df607bdd9b646e4667
KevinDaPlayer <325881@gmail.com>
2022-04-29 16:07:28 +0800

    Orderr

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,17 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
+    int shipFeed;
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        shipFeed = delivery.price;
+    }
+    public int total () {
+        if (delivery.name == "Shopee" && amount >= 199 ){
+            shipFeed = 0;
+        }
+        return amount + shipFeed;
     }
 }
